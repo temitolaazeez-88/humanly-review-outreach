c7f96814ffcaae020a70255e414c98d950690603
temitolaazeez-88 <temitolaazeez@gmail.com>
2026-09-13 12:02:15 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -6,5 +6,5 @@
  */
 const HR_CONFIG = {
   // Paste your Google Apps Script Web App URL here (ends in /exec).
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbwiSZt-mZ79sbdY46Woj-Au3zAZoumUA6sHYdroxyIcI1mOZHD3dwd4Yod0wJJJfb3X/exec"
 };
